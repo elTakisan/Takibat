01b43b07c8ca5854a1873e4b1dd8045917171f12
Jonah el Pelirrojo <takisan@pelirrojo.ninja>
2015-03-01 11:22:47 -0600

    Trabaje Otra Vez

--- python/example.py
+++ python/example.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from ninja.pelirrojo.takibat.bot import BotPlugin,BotCommand,BotThread
+from ninja.pelirrojo.takibat.bot import BotPlugin,BotCommand
 
 # The Difference between a Command and a Plugin is that a Command
 # is only handed execution when it is invoked, and a Plugin is
